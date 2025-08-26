efd0b8197ca0835461d9abe28e0d9cc166741b20
Josef Bauer <josef.bauer.1st@gmail.com>
2025-08-27 00:16:10 +0200

    Fix detekt findings

--- backend/src/main/kotlin/com/jotoai/voenix/shop/image/web/dto/ImageGenerationRequest.kt
+++
@@ -1,9 +0,0 @@
-package com.jotoai.voenix.shop.image.web.dto
-
-data class ImageGenerationRequest(
-    val promptId: Long,
-    val cropX: Double? = null,
-    val cropY: Double? = null,
-    val cropWidth: Double? = null,
-    val cropHeight: Double? = null,
-)
