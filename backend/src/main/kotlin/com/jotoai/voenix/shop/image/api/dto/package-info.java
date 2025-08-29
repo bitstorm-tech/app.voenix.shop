19dca8622716593e299f61a84de6630a2d574364
Josef Bauer <josef.bauer.1st@gmail.com>
2025-08-30 00:12:58 +0200

    Add NamedInterface annotations

--- backend/src/main/kotlin/com/jotoai/voenix/shop/image/api/dto/package-info.java
+++ backend/src/main/kotlin/com/jotoai/voenix/shop/image/api/dto/package-info.java
@@ -2,4 +2,5 @@
  * Data Transfer Objects for the image module API.
  * These DTOs define the contracts for data exchange between the image module and external clients.
  */
+@org.springframework.modulith.NamedInterface("dto")
 package com.jotoai.voenix.shop.image.api.dto;
