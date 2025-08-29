d6512380f9250cf9f77e2c72d3032138c1c4b360
Josef Bauer <josef.bauer.1st@gmail.com>
2025-08-29 23:19:53 +0200

    Update Modulith tests

--- backend/src/test/kotlin/com/jotoai/voenix/shop/application/ApplicationModulithTest.kt
+++ backend/src/test/kotlin/com/jotoai/voenix/shop/application/ApplicationModulithTest.kt
@@ -1,37 +1,23 @@
 package com.jotoai.voenix.shop.application
 
+import com.jotoai.voenix.shop.VoenixShopApplication
 import org.junit.jupiter.api.Test
 import org.springframework.modulith.core.ApplicationModules
 import org.springframework.modulith.docs.Documenter
 
 class ApplicationModulithTest {
-    private val modules = ApplicationModules.of(com.jotoai.voenix.shop.VoenixShopApplication::class.java)
+    private val modules = ApplicationModules.of(VoenixShopApplication::class.java)
 
     @Test
-    fun `should detect application module`() {
-        // Simple test to ensure the application module is detected by Spring Modulith
-        val applicationModule = modules.getModuleByName("application")
-        assert(applicationModule.isPresent) {
-            "Application module should be present in the application modules"
-        }
-
-        // Verify the module has the expected name (it gets detected as "Application Utilities" by default)
-        val module = applicationModule.get()
-        assert(module.displayName == "Application") {
-            "Expected 'Application', got '${module.displayName}'"
-        }
+    fun verifiesModularStructure() {
+        modules.verify()
     }
 
     @Test
     fun `should generate module documentation`() {
-        // Generate documentation for the module structure
-        try {
-            Documenter(modules)
-                .writeDocumentation()
-                .writeIndividualModulesAsPlantUml()
-        } catch (e: Exception) {
-            // Documentation generation might fail due to violations, but that's ok for this test
-            // The important thing is that the modules can be loaded
-        }
+        Documenter(modules)
+            .writeIndividualModulesAsPlantUml()
+            .writeModulesAsPlantUml()
+            .writeDocumentation()
     }
 }
