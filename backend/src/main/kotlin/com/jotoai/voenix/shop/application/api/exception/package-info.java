71052d17bdd701b7e16774838eeeb56d240334f9
Josef Bauer <josef.bauer.1st@gmail.com>
2025-08-30 10:58:42 +0200

    Remove CacheConfigImpl

--- backend/src/main/kotlin/com/jotoai/voenix/shop/application/api/exception/package-info.java
+++ backend/src/main/kotlin/com/jotoai/voenix/shop/application/api/exception/package-info.java
@@ -1,4 +1,5 @@
 /**
  * Application module base exceptions for cross-module usage
  */
+@org.springframework.modulith.NamedInterface("exception")
 package com.jotoai.voenix.shop.application.api.exception;
