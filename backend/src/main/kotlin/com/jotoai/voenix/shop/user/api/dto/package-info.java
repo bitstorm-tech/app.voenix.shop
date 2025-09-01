b55d6e1e6bb80e01f88957e6e49331ceb68e36c3
Josef Bauer <josef.bauer.1st@gmail.com>
2025-09-01 21:10:58 +0200

    Simplify user module

--- backend/src/main/kotlin/com/jotoai/voenix/shop/user/api/dto/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Data Transfer Objects for the User API.
- * <p>
- * This package contains all DTOs used for communication between the User module
- * and external clients or other modules.
- * </p>
- */
-package com.jotoai.voenix.shop.user.api.dto;
--- backend/src/main/kotlin/com/jotoai/voenix/shop/user/web/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Web adapters for the User module.
- * Contains REST controllers and other web-specific components.
- */
-package com.jotoai.voenix.shop.user.web;
